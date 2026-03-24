09741475c74250503269cf6f41161a6744ee8b35
NoahArno <1215664493@qq.com>
2026-03-24 10:14:42 +0800

    ui-expand宏适配

---
+++ confluence-xml/src/main/java/org/xwiki/contrib/confluence/filter/internal/macros/UIExpandMacroConverter.java
@@ -0,0 +1,76 @@
+/*
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.xwiki.contrib.confluence.filter.internal.macros;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+import org.apache.commons.lang3.StringUtils;
+import org.xwiki.component.annotation.Component;
+import org.xwiki.contrib.confluence.filter.AbstractMacroConverter;
+
+/**
+ * Convert Confluence ui-expand macro to XWiki expandable macro.
+ *
+ * @version $Id$
+ * @since 9.96.0
+ */
+@Component
+@Singleton
+@Named("ui-expand")
+public class UIExpandMacroConverter extends AbstractMacroConverter
+{
+    @Override
+    public String toXWikiId(String confluenceId, Map<String, String> confluenceParameters, String confluenceContent,
+        boolean inline)
+    {
+        return "expandable";
+    }
+
+    @Override
+    protected Map<String, String> toXWikiParameters(String confluenceId, Map<String, String> confluenceParameters,
+        String content)
+    {
+        Map<String, String> parameters = new LinkedHashMap<>(1);
+        saveParameter(confluenceParameters, parameters, "title", "summary", true);
+        return parameters;
+    }
+
+    @Override
+    protected String toXWikiContent(String confluenceId, Map<String, String> parameters, String confluenceContent)
+    {
+        String content = StringUtils.strip(StringUtils.defaultString(confluenceContent), "\r\n");
+        if (StringUtils.isEmpty(content)) {
+            return "\n";
+        }
+
+        // Keep one extra line break in expandable content.
+        return content + "\n";
+    }
+
+    @Override
+    public InlineSupport supportsInlineMode(String id, Map<String, String> parameters, String content)
+    {
+        return InlineSupport.NO;
+    }
+}
